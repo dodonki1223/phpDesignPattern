ffb3aa81cd14a9437d1c5ac1fdf0f5bd662584d9
dodonki1223 <make_an_effort_wish_come_true@yahoo.co.jp>
2018-09-18 23:50:57 +0900

    ソースにコメント文を追加

--- FactoryMethod/idCard.php
+++ FactoryMethod/idCard.php
@@ -2,11 +2,28 @@
 
 require_once('framework.php');
 
+
+/**
+ * IDカードクラス
+ *   IDカードの機能を提供する
+ *
+ */
 class IDCard extends Product
 {
 
+    /**
+     * 所有者名を保持する変数
+     * 
+     * @var string
+     */
     private $owner;
 
+   /**
+    * コンストラクタ
+    *   「所有者名のカードを作ります。」形式の文字列をコンソールに標示
+    *
+    * @param string $owner 所有者名
+    */
     public function __construct($owner)
     {
 
@@ -15,6 +32,11 @@ class IDCard extends Product
 
     }
 
+    /**
+     * 使用する
+     *   「所有者名のカードを使います。」形式の文字列をコンソールに標示
+     *
+     */
     public function use()
     {
 
@@ -22,20 +44,41 @@ class IDCard extends Product
 
     }
 
+    /**
+     * 所有者を取得
+     *   コンストラクタでセットされた所有者名を返す
+     *
+     */
     public function getOwner() 
     {
 
-        return $this->$owner;
+        return $this->owner;
 
     }
 
 }
 
+/**
+ * IDカードファクトリークラス
+ *   IDカードを作成する機能を提供する
+ *
+ */
 class IDCardFactory extends Factory
 {
 
+    /**
+     * 所有者名を保持する変数
+     * 
+     * @var string
+     */
     private $owners;
 
+    /**
+     * IDCardを作成する
+     *
+     * @param  string  $owner IDCardの所有者名
+     * @return Product IDCard（Productクラス）
+     */
     public function createProduct(string $owner): Product
     {
 
@@ -43,17 +86,28 @@ class IDCardFactory extends Factory
 
     }
 
+    /**
+     * 所有者を登録するメソッド
+     *
+     * @param Product $product Productクラス
+     */
     public function registerProduct(Product $product)
     {
 
-        $this->$owners[] = $product->getOwner(); 
+        // 所有者名を追加する
+        $this->owners[] = $product->getOwner();
 
     }
 
+    /**
+     * 所有者を登録するメソッド
+     *
+     * @return array 所有者名
+     */
     public function getOwners()
     {
 
-        $this->owners;
+        return $this->owners;
 
     }
 
